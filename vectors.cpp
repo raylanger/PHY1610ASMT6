9748c7bf3c035db97e445af9779fad76a0d4adb6
Raymond Langer <raymolanger@gmail.com>
2024-03-14 13:19:32 -0400

    Fixed vector functions - vectors were wrong size and initialized incorrectly

--- vectors.cpp
+++ vectors.cpp
@@ -6,33 +6,34 @@ rvector<double> get_d(int N){
 
     double dx = 1.0/N;
 
-    rvector<double> d(N);
+    rvector<double> d(N-1);
 
     double x = 0;
-    for (int i = 0; i < N; i++){
+    for (int i = 0; i < N-1; i++){
         x += dx;
-        std::cout<<x<<", " << epsilon(x) << "\n";
         double e_p = epsilon(x + dx/2.0);
         double e_m = epsilon(x - dx/2.0);
 
         d[i] = -(e_p+e_m);
     }
 
+    std::cout<<d<<"\n";
     return d;
 }
 
 rvector<double> get_offd(int N){
     double dx = 1.0/N;
 
-    rvector<double> off_d(N-1);
+    rvector<double> off_d(N-2);
 
     double x = 0;
-    for (int i = 0; i < N-1; i++){
+    for (int i = 0; i < N-2; i++){
         x += dx;
 
         off_d[i] = epsilon(x + dx/2.0);
     }
 
+    std::cout<<off_d<<"\n";
     return off_d;
 }
 
@@ -44,7 +45,11 @@ rvector<double> get_b(int N){
 
     double x = (N-1)*dx + dx/2.0;
 
-    b[N-1] = -epsilon(x); 
+    std::cout<<x<<"\n";
+
+    b[N-2] = -epsilon(x);  
+
+    std::cout<<b<<"\n";
 
     return b;
 }
